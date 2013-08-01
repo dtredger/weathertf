c0b500d96557641074b208b8fc19c48e74d7682b
dtredger <d.tredger@gmail.com>
2013-07-31 22:19:37 -0400

    new rails app without test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weathertf::Application.config.secret_key_base = 'd28f6c86943df0e793ecca84e479d8ad994538495837a16a9bf654f7b8a924ab59f5a9b38da22872bd16219b1219d0ec91eff2fac6ada450eeb5dc67e077e367'
