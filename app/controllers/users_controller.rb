f270ee36625d36c56036324e4b43c68e82171d3b
dtredger <d.tredger@gmail.com>
2013-08-04 17:49:29 -0400

    new user created with model on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,47 @@
 class UsersController < ApplicationController
 
-  def index
-    @user = User.new
-  end
+	def index
+		@user = User.new
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def new
-  end
+	def new
+	end
 
-  def create
-  end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to :back, notice: "well played"
+		else
+			flash.now[:notice] = "nope"
+			render :new
+		end
+	end
 
-  def edit
-  end
+	def edit
+		@user = User.find(params[:id])
+	end
 
-  def update
-  end
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attriputes(user_params)
+			redirect_to :back, notice: "updated"
+		else
+			render :edit, alert: "error!"
+		end
+	end
 
-  def delete
-  end
+	def delete
+	end
+
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username, :email, :phone_number, :carrier, :password)
+		end
+
 
 end
