27ed46e97da3f4a029493c719a4135be46e9a2e1
dtredger <d.tredger@gmail.com>
2013-08-21 14:40:47 -0400

    checks pw and confirm and phone #

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	authenticates_with_sorcery!
 
-	validates_presence_of :password, on: :create
+	validates_presence_of :password, length: {minimum: 3}, on: :create
 	validates_confirmation_of :password
 
 	validates_presence_of :phone_number
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,99 +23,18 @@ describe User do
 
   describe "phone num too short" do
     before { @user.phone_number = 123456789 }
+    it { should_not be_valid }
+  end
 
+  describe "no password" do
+    before { @user.password = @user.password_confirmation = "   " }
+    it { should_not be_valid }
+  end
+
+  describe "confirmation doesn't match" do
+    before { @user.password_confirmation = "different!" }
     it { should_not be_valid }
   end
 
 end
 
-  describe "no password" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
-  end
-
-#   describe "password too short" do
-#     before { @user.password = @user.password_confirmation = "a" * 5 }
-#     it { should be_invalid }
-#   end
-
-#   describe "confirmation doesn't match password" do
-#     before { @user.password_confirmation = "mismatch" }
-#     it { should_not be_valid }
-#   end
-
-#   describe "confirmation set to nil" do
-#     before { @user.password_confirmation = nil }
-#     it { should_not be_valid }
-#   end
-
-
-#   describe "return value of authenticate" do
-#     before { @user.save }
-#     let(:found_user) { User.find_by_email(@user.email) }
-
-#     describe "with valid password" do
-#       it { should == found_user.authenticate(@user.password) }
-#     end
-
-#     describe "invalid password" do
-#       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-#       it { should_not == user_for_invalid_password }
-#       specify { user_for_invalid_password.should be_false }
-#     end
-#   end
-
-
-#   describe "remember token" do
-#     before { @user.save }
-#     its(:remember_token) { should_not be_blank }
-#   end
-
-#   describe "with admin set to 'true' -" do
-#     before do
-#       @user.save!
-#       @user.toggle!(:admin)
-#     end
-
-#     it { should be_admin }
-#   end
-
-#   describe "micropost associations -" do
-#     before { @user.save }
-#     let!(:older_micropost) do
-#       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-#     end
-#     let!(:newer_micropost) do
-#       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-#     end
-
-#     it "should have newest post first" do
-#       @user.microposts.should == [newer_micropost, older_micropost]
-#     end
-
-#     it "should destroy associated microposts" do
-#       microposts = @user.microposts.dup
-#       @user.destroy
-
-#       microposts.should_not be_empty
-#       microposts.each do |micropost|
-#         Micropost.find_by_id(micropost.id).should be_nil
-#       end
-#     end
-
-#     describe "status" do
-#       let(:unfollowed_post) do
-#         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user) )
-#       end
-
-#       its(:feed) { should include(newer_micropost) }
-#       its(:feed) { should include(older_micropost) }
-#       its(:feed) { should_not include(unfollowed_post) }
-#     end
-
-#   end
-
-
-
-# end
