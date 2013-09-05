6a85b42ab414a15ec555c5e2775e792ba03b823c
dtredger <d.tredger@gmail.com>
2013-09-05 12:53:00 -0400

    pg requires phone num to be bigint

---
+++ db/migrate/20130905165008_change_phone_to_big_int.rb
@@ -0,0 +1,5 @@
+class ChangePhoneToBigInt < ActiveRecord::Migration
+  def change
+  	change_column :users, :phone_number, :bigint
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130830200009) do
+ActiveRecord::Schema.define(version: 20130905165008) do
 
   create_table "users", force: true do |t|
     t.string   "username",         null: false
