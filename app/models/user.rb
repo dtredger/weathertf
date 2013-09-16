3989037c93bf4109b1f380d1ea38a27175546351
dtredger <d.tredger@gmail.com>
2013-09-15 20:26:06 -0400

    took phone, carrier & username out of form; now username is first part of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,25 @@ class User < ActiveRecord::Base
 	validates_presence_of :password, on: :create
 	validates_confirmation_of :password
 
-	validates_presence_of :phone_number
-	validates :phone_number, length: {minimum: 10}, numericality: { only_integer: true }
 
-	validates_presence_of :carrier
+  # not implemented currently, so don't require them
+	#validates_presence_of :phone_number
+	#validates :phone_number, length: {minimum: 10}, numericality: { only_integer: true }
+
+	#validates_presence_of :carrier
+
+  before_validation :set_username
 
 
 	after_validation :reverse_geocode		# move into external process?
-		# :if => lambda { |user| user.city_changed? }  
+		# :if => lambda { |user| user.city_changed? }
+
+
+  private
+
+    def set_username
+      self.username = self.email.split("@")[0]
+    end
 
 
 end
