27a205415216d92c6cf11734a9631f8da24e55d4
dtredger <d.tredger@gmail.com>
2013-09-21 17:31:14 -0400

    resque initializer fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,5 @@
-require 'open-uri'
 uri = URI.parse(ENV["REDISCLOUD_URL"])
-$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 # username, password, hostname and port
 # redis://rediscloud:rVj76dW6cYOeN1IT@pub-redis-14711.us-east-1-4.3.ec2.garantiadata.com:14711
