8571675a002d018bd43382e6034a6bd751149b9a
dtredger <d.tredger@gmail.com>
2013-09-21 19:41:21 -0400

    unicorn allows several workers single web dyno

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 #uri = URI.parse(ENV["REDISTOGO_URL"])
 #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-REDIS = Redis.connect(:url => ENV['REDISTOGO_URL'])
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
