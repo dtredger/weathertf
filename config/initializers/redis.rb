f2949d622cd8ad6647a64925a2f4e378c0a79f51
dtredger <d.tredger@gmail.com>
2013-09-21 19:16:40 -0400

    redistogo connect

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#uri = URI.parse(ENV["REDISTOGO_URL"])
+#REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+REDIS = Redis.connect(:url => ENV['REDISTOGO_URL'])
