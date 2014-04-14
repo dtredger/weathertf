b74f91f9d659c887b78d9448f37406d7361906c7
dtredger <d.tredger@gmail.com>
2014-04-14 14:37:34 -0400

    generate mailer creates spec

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe UserMailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe "welcome_email" do
+   	pending
+  end 
+  
+
+  describe "daily_email" do
+   	pending
+  end 
+
+  describe "reset_password_email" do
+   	pending
+  end 
+
+
+
+end
