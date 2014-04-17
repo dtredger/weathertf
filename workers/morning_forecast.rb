857aecb6a740902d9733351481ed5b1b41fa2933
dtredger <d.tredger@gmail.com>
2014-04-16 20:30:55 -0400

    extract current_forecast from application_controller

--- workers/morning_forecast.rb
+++ workers/morning_forecast.rb
@@ -13,5 +13,30 @@ def get_forecast(user_id)
   u = User.find(user_id)
   latitude = u.latitude
   longitude = u.longitude
-  f = ForecastIO.forecast(latitude, longitude)
+  ForecastIO.forecast(latitude, longitude)
+end
+
+def parse_forecast(forecast_json)
+end
+
+def get_current_forecast
+  @forecast = ForecastIO.forecast(
+    current_user.latitude,          #latitude
+    current_user.longitude,         #longitude
+    time: Time.now.to_i,    #new(2013, 7, 31).to_i,
+    params: {
+      units: 'si',
+      exclude: 'flags'
+    })
+  
+  @hourly = @forecast.hourly.data
+  @daily = @forecast.daily.data.first
+
+  @future_hrs = []
+
+  @hourly.each do |h|
+    if h.time > Time.now.to_i
+      @future_hrs << h
+    end
+  end
 end
