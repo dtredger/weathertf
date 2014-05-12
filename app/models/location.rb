4676fdf50b14f3a3f6a384a4922f36a1701e448f
dtredger <d.tredger@gmail.com>
2014-05-12 15:01:08 -0400

    Location model but does not geocode yet

---
+++ app/models/location.rb
@@ -0,0 +1,45 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  latitude   :float
+#  longitude  :float
+#  address    :string(255)
+#  user_id    :integer
+#
+
+class Location < ActiveRecord::Base
+  belongs_to :user
+
+  validate :address_or_coordinates
+
+  private
+
+    def address_or_coordinates
+      if (latitude.blank? or longitude.blank?)
+        validates_presence_of :address
+      else
+        validates_presence_of :latitude, numericality: true
+        validates_presence_of :longitude, numericality: true
+      end
+    end
+end
+
+
+
+# class ForwardLocation < Location
+#   geocoded_by :address
+#   after_validation :geocode
+
+# end
+
+
+# class ReverseLocation < Location
+#   reverse_geocoded_by :latitude, :longitude,
+#     address: :address
+#   after_validation :reverse_geocode
+  
+# end
